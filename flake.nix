fb4d86d66c84a2558aef280d0bb9fb4b2b2fffa2
Edward Tang <tanged@umich.edu>
2026-02-15 19:14:45 +0000

    bumpversion

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
         # Hermes package
         hermesPackage = pkgs.python3Packages.buildPythonPackage {
           pname = "hermes";
-          version = "0.2.0";
+          version = "0.3.0";
           src = ./.;
           format = "pyproject";
 
