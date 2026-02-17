031973e53c8ef994a4b1a66e228b6ea31c3091d1
Edward Tang <tanged@umich.edu>
2026-02-17 07:19:53 +0000

    bump icarus and version

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
         # Hermes package
         hermesPackage = pkgs.python3Packages.buildPythonPackage {
           pname = "hermes";
-          version = "0.3.2";
+          version = "0.3.3";
           src = ./.;
           format = "pyproject";
 
